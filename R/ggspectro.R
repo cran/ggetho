77b86b89642b921276c1346266e9f046115448dc
Quentin Geissmann <qgeissmann@gmail.com>
2020-04-05 14:30:05 +0000

    version 0.3.5

--- R/ggspectro.R
+++ R/ggspectro.R
@@ -11,13 +11,16 @@
 #' data(dams_sample)
 #' dt <- dams_sample
 #' spect_dt <- spectrogram(activity, dt)
-#' pl <- ggspectro(spect_dt,time_wrap = hours(24)) + stat_tile_etho() + scale_y_hours(log=T) +
+#' pl <- ggspectro(spect_dt,time_wrap = hours(24)) +
+#'   stat_tile_etho() +
+#'   scale_y_hours(log=TRUE) +
 #'   stat_ld_annotations(ld_colours = c("grey","black"))
 #' pl + facet_grid(period_group ~ .)
 #' pl + facet_wrap(~ id)
 #'}
 #'@seealso
 #' * [ggperio] to visualise periodograms
+#' @return An initial ggplot object that can be further edited.
 #'@references
 #' * The relevant [rethomic tutorial section](https://rethomics.github.io/ggetho.html#spectrogram)
 #' @export
